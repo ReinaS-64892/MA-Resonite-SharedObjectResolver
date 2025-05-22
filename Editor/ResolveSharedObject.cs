4511f44dcf793763e410bdd9b711ab6844361de4
Reina_Sakiria <rs@rs64.net>
2025-05-22 18:38:42 +0900

    chore: add libmsdfgen.so

--- Editor/ResolveSharedObject.cs
+++ Editor/ResolveSharedObject.cs
@@ -51,7 +51,7 @@ namespace net.rs64.MAResonite.SharedObjectResolver
             Task.WaitAll(NeedSharedObjects().Select(a => GetLibFromZip(a.url, a.libEntry)).ToArray());
         }
 
-        internal const int ResolveBumpVersion = 1;
+        internal const int ResolveBumpVersion = 2;
         static IEnumerable<(string url, IEnumerable<string> libEntry)> NeedSharedObjects()
         {
             yield return (
@@ -76,6 +76,10 @@ namespace net.rs64.MAResonite.SharedObjectResolver
             yield return (
                 "https://www.nuget.org/api/v2/package/Brotli.NET/2.1.1",
                 new[] { "runtimes/linux-x64/native/brolib_x64.so" }
+                );
+            yield return (
+                "https://github.com/ReinaS-64892/msdfgen-ydms-binary/releases/download/202505220923/msdf-gen-ydms-binary.zip",
+                new[] { "build/out/libmsdfgen.so" }
                 );
         }
     }
